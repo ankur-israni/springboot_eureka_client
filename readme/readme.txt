902ec321bb85f46cf1dc857a250c38eb0e446354
Ankur Israni <ankur.israni@aa.com>
2020-08-23 12:18:06 -0500

    all done

---
+++ readme/readme.txt
@@ -0,0 +1,9 @@
+1) Eureka Client URI
+--------------------------
+http://localhost:7000/swagger-ui.html
+
+2) Eureka Server URI
+--------------------------
+http://localhost:9006
+
+3) Start the Eureka Server before you start the eureka client
--- src/main/java/com/ankur/eureka/boot/InventoryApplication.java
+++ src/main/java/com/ankur/eureka/boot/InventoryApplication.java
@@ -3,8 +3,10 @@ package com.ankur.eureka.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = {"com.ankur"})
+@EnableEurekaClient
 public class InventoryApplication {
 
     public static void main(String[] args) {
